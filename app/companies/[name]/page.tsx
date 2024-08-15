02af0538c11325ba5c87d7075d0362fbb580e5ba
sudheer0071 <sudheer1614@gmail.com>
2024-08-16 05:21:56 +0530

    Responsive bits done

--- app/companies/[name]/page.tsx
+++ app/companies/[name]/page.tsx
@@ -7,8 +7,10 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Loader } from "../../components/Loader";
 import axios from 'axios'
-import { BACKEND_URL } from "@/app/lib/config";
+import { BACKEND_URL, R2 } from "@/app/lib/config";
 import { Pagination } from "@/app/components/Pagination";
+import { useRecoilState } from "recoil";
+import { productNameState } from "@/app/recoilContextProvider";
  
 const Company:NextPage = ()=>{
   // const [company, setCompany] = useState<string | string[] | undefined>();
@@ -18,18 +20,21 @@ const Company:NextPage = ()=>{
   const [loading, setLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
   const [companies, setCompanies] = useState({
-    name:'',
-    image:'',
-    header:'',
-    about:['',' ',' '],
+    
+manufacturer
+:{
+  company_name:'',
+      img:[{image:''}],
+      about:['',' ',' '],
+    }, 
     products:[
       {
-        model:'',
+        name:'',
         image:'',
       }
     ]
   })
-  let pageSize = 12;
+  let pageSize = 6;
     const Companies =   {
       companyName:"Mitsubishi",
       image:"2020/03/S02-pic1.jpg",
@@ -463,8 +468,10 @@ const Company:NextPage = ()=>{
   const currentProducts = useMemo(()=>{
     const firstPageIdx = (currentPage - 1) * pageSize;
     const lastPageIdx = pageSize + firstPageIdx;
-    return Companies.products.slice(firstPageIdx, lastPageIdx);
-  },[currentPage])
+    console.log(firstPageIdx, lastPageIdx);
+    
+    return companies.products.slice(firstPageIdx, lastPageIdx);
+  },[currentPage, companies.products])
 
     const sendReq = async () => {
       try{
@@ -491,7 +498,7 @@ const Company:NextPage = ()=>{
       }, 2000);
     },[])
      
-  if (company!='Mitsubishi') {
+  if (company!=name.name) {
     return <div className=" ">
     <Loader/>
   </div>
@@ -499,34 +506,36 @@ const Company:NextPage = ()=>{
   return <div>
     <div> 
     </div>
-    <TopCard label={Companies.companyName} />
-      <div className=" bg-white pt-32 pb-20 m-auto ">
-        <div className=" text-4xl text-black font-semibold flex justify-center">
-          {Companies.header} 
+    <TopCard label={companies.manufacturer.company_name} />
+      <div className=" bg-white pt-32 pb-20 md:m-auto lg:m-auto ">
+        <div className=" mx-6 md:mx-5 lg:mx-0 text-2xl md:text-3xl lg:text-4xl text-black font-semibold flex justify-center">
+         A Professional {companies.manufacturer.company_name} distrubutor
         </div>
-        <div id="comp" className=" textblack py-12 mt-12 ">
-          <div className=" px-4 max-w-7xl w-full mx-auto">
-          <div className=" flex">
-          <div className=" z-0 hd"> 
-            <img className="shadow-2xl ml-10" src={`https://www.plc-sensors.com/wp-content/uploads/${Companies.image}}`} alt="" /> 
+        <div id="comp" className="  textblack py-8 mt-3 md:py-10 md:mt-10 lg:py-12 lg:mt-12 ">
+          <div className=" px-4 lg:max-w-7xl w-full  lg:mx-auto">
+          <div className=" lg:flex">
+          <div className=" z-0 hd flex justify-center"> 
+            <img className="shadow-2xl lg:ml-10" src={`${R2}${companies.manufacturer.img[0].image}`} alt="" /> 
           </div>
-          <div className=" ml-24 max-w-2xl font-light text-black">
-            {Companies.about.map((about, idx)=><p className=" mt-8" key={idx}>{about}</p>)} 
+          <div>
+          <div className=" mx-2 sm:mx-5 md:mx-7 lg:mx-0 lg:ml-24 lg:max-w-2xl font-light text-black">
+            {companies.manufacturer.about.map((about, idx)=><p className=" mt-8" key={idx}>{about}</p>)} 
+          </div>
           </div>
           </div>
           </div>
         </div>
       </div>
 
-      <div className="pt-20 pb-10 bg-white text-black">
-        <div className=" flex flex-col mx-32">
-          <div className=" text-4xl w-full font-semibold max-w-4xl">
-          We carry the following products from {Companies.companyName} but not limited to
+      <div className=" md:pt-14 md:pb-5 lg:pt-20 lg:pb-10 bg-white text-black">
+        <div className=" flex flex-col mx-6 sm:mx-14 md:mx-24 lg:mx-32">
+          <div className=" text-2xl md:text-3xl lg:text-4xl w-full font-semibold max-w-4xl">
+          We carry the following products from {companies.manufacturer.company_name} but not limited to
           </div> 
-          <div className=" mt-10 grid grid-cols-4"> 
-            {currentProducts.map((product,idx)=><Products name={JSON.stringify(name)} key={idx} model={product.model} image={product.image} />) } 
+          <div className=" mt-10 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4"> 
+            {currentProducts.map((product,idx)=><Products name={JSON.stringify(name)} key={idx} model={product.name} image={product.image} />) } 
           </div>
-        <Pagination currentPage={currentPage} totalCount={Companies.products.length} onPageChange={(page:number)=>setCurrentPage(page)} pageSize={pageSize} />
+        <Pagination currentPage={currentPage} totalCount={companies.products.length} onPageChange={(page:number)=>setCurrentPage(page)} pageSize={pageSize} />
 
         </div> 
       </div>
@@ -534,13 +543,18 @@ const Company:NextPage = ()=>{
 }
 
 
-const Products = ({name,model, image}:{name:string, model:string, image:string})=>{
-  return <div>
-      <Link href={`/companies/${name}/${encodeURIComponent(model.split(' ')[1])}`}> 
+const Products = ({name,model, image}:{name:string, model:string, image:string})=>{ 
+  
+  const [productName, setProductName] = useRecoilState(productNameState)
 
-    <div className=" p-4 mr-8 shadow-lg hover:shadow-xl hover:scale-110 ml-6 hover:text-blue-500 cursor-pointer transition-all duration-500"> 
-        <img className=" rounded-lg  transition-all duration-500" src={`https://www.plc-sensors.com/wp-content/uploads/${image}`} alt="" />
-        <div className=" mt-2 text-center text-xl font-semibold">
+  return <div>
+      <Link onClick={()=>setProductName(model)} href={`/companies/${name}/${encodeURIComponent(model.split(' ')[0])}`}> 
+
+    <div className=" mt-2 p-2 mr-3 md:p-2 md:mr-4 lg:p-4 lg:mr-8 shadow-lg hover:shadow-xl hover:scale-110 ml-3 md:ml-4 lg:ml-6 hover:text-blue-500 cursor-pointer transition-all duration-500"> 
+      <div className=" h-full">
+        <img className=" rounded-lg  transition-all duration-500 " src={`${R2}${image}`} alt={model} />
+      </div>
+        <div className=" mt-2 text-center text-sm md:text-lg lg:text-xl font-semibold">
           {model}
         </div>
     </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { act, useRef, useState } from "react";
+import React, { act, useEffect, useRef, useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "../lib/utils";
 import { Button } from "./ui/Button";
@@ -7,11 +7,12 @@ import Link from "next/link";
 import { EnquiryCard } from "./cards/EnquiryCard";
 import { useRecoilState } from "recoil";
 import {  quoteState, sendEnquiryState, showSearchState } from "../recoilContextProvider";
-import { Search } from "lucide-react";
+import { ExternalLink, Link2Icon, MenuIcon, Search } from "lucide-react";
 import { countries } from "./sample";
 import axios from "axios";
 import { BACKEND_URL } from "../lib/config";
 import { Loader } from "./Loader";
+import { motion } from "framer-motion";
  
 export default function Navbar() {
   
@@ -29,7 +30,8 @@ function NavbarCheck({ className }: { className?: string }) {
   const [sendEnquiry, setSendEnquiry] = useRecoilState(sendEnquiryState)
   const[start,setStart] = useState(false) 
   const [showSearch, setShowSearch] = useRecoilState(showSearchState)
-
+  const [list, setList] = useState([])
+  const [responseiveNav, setResponsiveNav] = useState(false)
   const[searchTerm, setSearchTerm] = useState('')
   const[searchResult, setSearchResult] = useState<[{company:string [], product:string[]}]>([
     {
@@ -69,27 +71,6 @@ const getData = async (value:string)=>{
    console.log(result);
 }
 
-// Main call the backend API
-const getSearched = async (value:string)=>{
-  if (value.length==0 || value==' ') {
-    setStart(false)
-    setSearchResult([{company:[''], product:[]}])
-    return;
-  }
-   const res  = await axios.get(`${BACKEND_URL}/products/search/?query=${value}`) 
-  const result = res.data.map(({ details }:any) => details);
-  const companies = res.data.map(({ manufacturer }:any)=>manufacturer);
-  const company = companies.map((mfg:any)=>mfg.company_name);
-  const filteredRes = result.map((name:string[]|any)=>name.map((nme:any)=>nme.value))
- 
-  console.log(res.data);
-  console.log(result);
-  console.log(company);
-  console.log(filteredRes); 
- setSearchResult([{company:company, product:filteredRes}])
-}
-
-
 // debounce function
 function debounce (func:any, delay:number){
   return function(...args:[]){
@@ -103,14 +84,37 @@ function debounce (func:any, delay:number){
 
 
 // Debounce  Search function
-const debounceSearch =(value:string)=> debounce(getSearched(value),DELAY)
+const debounceSearch= (value:string) => debounce(getSearched(value), DELAY);
 
+// Main call the backend API
+const getSearched = async (value:string)=>{
+  if ( value.length==0 || value==' ') {
+    setStart(false)
+    setSearchResult([{company:[''], product:[]}])
+    return;
+  }
+  else{
+    const res  = await axios.get(`${BACKEND_URL}/products/search/?query=${value}`) 
+   const result = await res.data.map(({ details }:any) => details);
+   const companies = res.data.map(({ manufacturer }:any)=>manufacturer);
+   const company = companies.map((mfg:any)=>mfg.company_name);
+   const filteredRes = result.map((name:string[]|any)=>name.map((nme:any)=>nme.value)).sort()
+  
+   console.log(res.data);
+   console.log(result);
+   console.log(company);
+   console.log(filteredRes); 
+  setSearchResult([{company:company, product:filteredRes}])
+  }
+}
+
+
+ 
 const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
   const { value } = e.target
   setSearchTerm(value)
   setShowSearch(true)
-  setStart(true)
-//  getData(value)
+  setStart(true) 
   debounceSearch(value)
 }
  
@@ -132,7 +136,7 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
 
       console.log(item.product.length);
       return <div key={idx2}>
-         <Link onClick={()=>setSearchTerm('')} href={`/companies/${companyName}/${item.product[idx2][1]}`} key={idx2} >
+         <Link onClick={()=>{setSearchTerm(''); setResponsiveNav(!responseiveNav)}} href={`/companies/${companyName}/${item.product[idx2][1]}`} key={idx2} >
     <div className=" hover:bg-slate-100 p-2 hover:scale-105 hover:text-slate-800 transition-all duration-500 cursor-pointer mt-2 border-b-2 "> {companyName}, {item.product[idx2]}  </div> 
      </Link>
       </div>
@@ -142,46 +146,165 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
   
 const companies =["pro-face", "Mitsubishi", "omron", "fanux", "delta", "hITECH", "yashkawa", "keyence", "sick", "panacsonic", "intek/seinvi", "schneider", "siemens", "hongfa"]
 
+useEffect(()=>{
+  sendReq()
+},[])
+  
+const sendReq = async () => { 
+  const res = await axios.get(`${BACKEND_URL}/companies/all`)
+  const response = res.data 
+  console.log("nav");
+  const lists = response.map((comp:any)=> comp.company_name)
+  setList(lists) 
+  console.log(lists);
+  
+  // setCompany(response)
+}
+
   return (
     <div
       className={cn("fixed flex-none inset-x-11 mx-aukto w-full z-50", className)}
     >
       <div className="flex h-20">
           
-        <div className=" bg-white shadow-md flex "> 
+        <div className=" bg-white shadow-md w-full"> 
+          <div className=" flex p-3 md:p-0 lg:p-0  ">
           <div className=" p-1 px-2 cursor-pointer" >
             <Link href={"/"}>
             <img width={80} src="https://5.imimg.com/data5/SELLER/Logo/2023/8/332359348/WL/RS/SO/102816454/logo-90x90.png" alt="" />
             </Link>
           </div>
 
-          <div className=" cursor-pointer text-black text-2xl text-dcenter mt-4 fldex items-center w-[500px] font-medium">
+          <div className="  text-black text-xl md:text-xl lg:text-2xl text-dcenter mt:mt-4 lg:mt-4 fldex items-center w-[500px] font-medium">
          <Link href={"/"}>
             Global Electronics Solutions
          </Link>
-            <div className=" -mt-3">
-              <small className=" text-xs">Global Electronic Solutions, Gurgaon, Gurugram, Haryana</small>
+            <div className="  ">
+              <div className=" text-xs max-w-xs md:max-w-sm lg:max-w-sm">Global Electronic Solutions, Gurgaon, Gurugram, Haryana</div>
             </div>
           </div>
-        </div>
+          <div onClick={()=>{ setResponsiveNav(!responseiveNav);setShowSearch(false);setSearchTerm('') }} className=" md:hidden lg:hidden cursor-pointer text-black">
+           <MenuIcon size={45}/>
+        </div> 
+          </div>
+          <motion.div
+          initial = {{scale:0.5,x:0, y:-250, opacity:0}}
+          whileInView={{scale:1,x:0, y:0, opacity:1}}
+          
+          transition={{ 
+           type:"just",
+           stiffness:200,
+           damping:7,
+           duration:0.5
+          }} 
+            
+           className={`${responseiveNav?'':'hidden'} text-black -mt-2 border-t-4 pt-3 justify-center bg-white w-full pb-3 shadow-md`}>
+          <div className=" p-3">
+          <div className=" relative flex justify-center flex-col items-center -mt-2 w-full">
+        <div className="flex flex-col  w-full items-center relative text-black">
+  <input  
+    className="border shadow-md shadow-gray-400 p-3 w-full rounded-md bg-slate-100 text-black font-medium text-md pl-5" 
+    placeholder="Enter Product Number or company" 
+    type="text" 
+    value={searchTerm}
+    onChange={handleInputChange}
+  /> 
+  <Search 
+    className="absolute right-2 top-6 transform -translate-y-1/2 text-gray-400"
+  /> 
+  <div className={`${showSearch}hdidden w-full`}>
+{{ searchResultEmpty } && showSearch && (
+  <div className={` ${!start?' hidden':''} w-full bg-white pt-3 rounded-b-lg border-gray-400 text-black font-medium `}>
+  <ul className="text-left border-2 text-md">
+    {searchResultList.map((res:any, idx:number)=><div key={idx} className=" border-t p-2 text-gray-500  translate-all duration-500 ">
+      {res}
+    </div>)} 
+  </ul>
+  </div>
+)}
 
-        <div className=" w-full">
-          <Menu setActive={setActive}>
-            <MenuItem setActive={setActive} active={active} item={"Products"}>
+ {/* {  debounceTimeout.current && <div>
+ <Loader /> loading
+ </div>} */}
+{searchResultEmpty && showSearch &&
+  searchTerm.length>0 && 
+  !debounceTimeout.current && (
+    <div className=" w-full shadow-lg border-2 rounded-b-lg flex justify-center items-center bg-white">
+      <div className="h-80 p-3 text-lg text-center items-center flex  ">
+      <div className=" ">No result found
+        <div className=" ">
+      <Button sendEnq={true} onclick={()=>{toggleSendEnquiryCard(); setResponsiveNav(!responseiveNav)}} height={2} label={"Send an Enquiry"} productCard={false} /> 
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+} 
+  </div>
+</div>
+
+          <div className=" mt-5 ">
+            <Button quote={true} nav={true} label={"Request a quote "} height={12} onclick={toggleEnquiryCard} productCard={false} />
+          </div>
+        </div> 
+          </div>
+            <div className=" flex justify-center w-full">
+          <Menu setActive={setActive2}> 
+            {/* <MenuItem setActive={setActive} active={active} item={"Products"}>
               <div className="flex flex-col space-y-4 text-sm">
                 <HoveredLink href="/web-dev">Web Development</HoveredLink>
                 <HoveredLink href="/interface-design">Interface Design</HoveredLink>
                 <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
                 <HoveredLink href="/branding">Branding</HoveredLink>
               </div>
-            </MenuItem>
+            </MenuItem> */} 
+            <MenuItem setActive={setActive2} active={active2} item={"Companies"}>
+              <div onClick={()=>{}} className=" flex flex-col space-y-5 text-sm">
+                {list.map((company, idx) => { 
+                  // const formattedCompany = company.comapny_name.charAt(0).toUpperCase() + company.company_name.slice(1).toLowerCase();
+                  return (
+                    <HoveredLink onClick={()=>{setActive2(''); setResponsiveNav(!responseiveNav)}} key={idx} href={`/companies/${company }`}>
+                      {company}
+                    </HoveredLink>
+                  );
+                })}
+
+              </div>
+            </MenuItem>  
+            <div className=" py-3">
+
+            </div>
+            <MenuItem setActive={setActive2} active={active2} item="About">
+              <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/hobby">about us  <ExternalLink size={20} className=" ml-1 inline"/>
+ 
+                </HoveredLink> 
+              </div>
+            </MenuItem> 
+  
+          </Menu>
+            </div>
+     
+          </motion.div>
+        </div>
+
+        <div className=" hidden lg:inline">
+          <Menu setActive={setActive}>
+            {/* <MenuItem setActive={setActive} active={active} item={"Products"}>
+              <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+                <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+                <HoveredLink href="/branding">Branding</HoveredLink>
+              </div>
+            </MenuItem> */}
             <MenuItem setActive={setActive} active={active} item={"Companies"}>
               <div onClick={()=>{}} className=" flex flex-col space-y-4 text-sm">
-                {companies.map((company, idx) => {
-                  const formattedCompany = company.charAt(0).toUpperCase() + company.slice(1).toLowerCase();
+                {list.map((company, idx) => { 
+                  // const formattedCompany = company.comapny_name.charAt(0).toUpperCase() + company.company_name.slice(1).toLowerCase();
                   return (
-                    <HoveredLink onClick={()=>setActive('')} key={idx} href={`/companies/${company}`}>
-                      {formattedCompany}
+                    <HoveredLink onClick={()=>setActive('')} key={idx} href={`/companies/${company }`}>
+                      {company}
                     </HoveredLink>
                   );
                 })}
@@ -190,22 +313,19 @@ const companies =["pro-face", "Mitsubishi", "omron", "fanux", "delta", "hITECH",
             </MenuItem>  
             <MenuItem setActive={setActive} active={active} item="About">
               <div className="flex flex-col space-y-4 text-sm">
-                <HoveredLink href="/hobby">Hobby
+                <HoveredLink href="/hobby">about us  <ExternalLink size={20} className=" ml-1 inline"/>
  
-                </HoveredLink>
-                <HoveredLink href="/individual">Individual</HoveredLink>
-                <HoveredLink href="/team">Team</HoveredLink>
-                <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+                </HoveredLink> 
               </div>
             </MenuItem> 
  
           </Menu>
         </div>
-        <div className=" relative -mt-2 w-full flex justify-end shadow-md bg-white">
-        <div className="flex flex-col mt-5 items-center mr-6 relative text-black">
+        <div className=" hidden md:flex lg:flex relative -mt-2 w-full  justify-end shadow-md bg-white">
+        <div className="flex flex-col mt-5 w-full items-center mr-6 relative text-black">
   <input  
-    className="border shadow-md shadow-gray-400 p-3 w-80 rounded-md bg-slate-100 text-black font-medium text-md pl-5" 
-    placeholder="Enter Product Number" 
+    className="border shadow-md shadow-gray-400 p-3 w-full rounded-md bg-slate-100 text-black font-medium text-md pl-5" 
+    placeholder="Enter Product Number or company" 
     type="text" 
     value={searchTerm}
     onChange={handleInputChange}
@@ -224,9 +344,9 @@ const companies =["pro-face", "Mitsubishi", "omron", "fanux", "delta", "hITECH",
   </div>
 )}
 
- {  debounceTimeout.current && <div>
+ {/* {  debounceTimeout.current && <div>
  <Loader /> loading
- </div>}
+ </div>} */}
 {searchResultEmpty && showSearch &&
   searchTerm.length>0 && 
   !debounceTimeout.current && (
@@ -247,7 +367,7 @@ const companies =["pro-face", "Mitsubishi", "omron", "fanux", "delta", "hITECH",
           <div className=" -mt-1 -ml-3">
             <Button quote={true} nav={true} label={"Request a quote "} height={2} onclick={toggleEnquiryCard} productCard={false} />
           </div>
-        </div>
+        </div> 
       </div>
       {showEnquiryCard && <EnquiryCard quote={ true}  />}
       {sendEnquiry && <EnquiryCard sendEnq={ true}  />}
